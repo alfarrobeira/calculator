5287be1974d7f8bbaf584b18d47c3d2855a91a6d
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-02-20 16:37:51 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,120 @@
+class Calculator {
+  // private fields ----------------------
+  #calculatorScreen = document.querySelector("#calculator .screen");
+  #calculatorButtons = document.querySelectorAll("#calculator span");
+  #clearButton = document.querySelector("#calculator .clear");
+  #equalsButton = document.querySelector("#calculator .eval");
+  #clearHistoryButton = document.querySelector("#calculator .clear-history");
+  // array for keeping the screen entries
+  #history = [];
+
+  // constructor -------------------------
+  constructor() {
+    this.#addListeners();
+  }
+
+  // public properties
+  get calculatorScreenText() {
+    return this.#calculatorScreen.innerText;
+  }
+
+  set calculatorScreenText(val) {
+    this.#calculatorScreen.innerText = val;
+  }
+
+  // private methods ---------------------
+
+  #addListeners() {
+    // listen to every key on the calculator and call function to add the value on the screen
+    this.#calculatorButtons.forEach((key) => {
+      if (key.innerText !== "=") {
+        key.addEventListener("click", (e) => this.#print(e.target.innerText));
+      }
+    });
+
+    // listen to click on clear key
+    this.#clearButton.addEventListener("click", () => this.#clearInput());
+
+    // listen to click on "=" key
+    this.#equalsButton.addEventListener("click", () =>
+      this.#evaluate(this.calculatorScreenText)
+    );
+
+    // listen to click on clear history button
+    this.#clearHistoryButton.addEventListener("click", () =>
+      this.#clearHistory()
+    );
+  }
+
+  // todo: call from destructor
+  #removeListeners() {
+    // listen to every key on the calculator and call function to add the value on the screen
+    this.#calculatorButtons.forEach((key) => {
+      if (key.innerText !== "=") {
+        key.removeEventListener("click", (e) =>
+          this.#print(e.target.innerText)
+        );
+      }
+    });
+
+    // remove EventListener from to click on clear key
+    this.#clearButton.removeEventListener("click", () => this.#clearInput());
+
+    // listen to click on "=" key
+    this.#equalsButton.removeEventListener("click", () =>
+      this.#evaluate(this.calculatorScreenText)
+    );
+
+    // listen to click on clear history button
+    this.#clearHistoryButton.removeEventListener("click", () =>
+      this.#clearHistory()
+    );
+  }
+
+  // returns the result of the provided expression (using the 'eval' function)
+  #evaluate(expression) {
+    if (expression.includes("x")) expression = expression.replaceAll("x", "*");
+
+    // add input to history
+    this.#addHistory(expression);
+
+    // evaluate input and show result
+    this.calculatorScreenText = eval(expression);
+  }
+
+  // write the value of the pressed key on the screen
+  #print(val) {
+    this.calculatorScreenText += val;
+  }
+
+  // add the input from the screen to the history array and update view
+  #addHistory(input) {
+    this.#history.push(input);
+    this.#updateHistoryView();
+  }
+
+  // clear input
+  #clearInput() {
+    this.calculatorScreenText = "";
+  }
+
+  // clear history array and update view
+  #clearHistory() {
+    this.#history = [];
+    this.#updateHistoryView();
+  }
+
+  #updateHistoryView() {
+    let list = document.querySelector("#calculator .bottom ul");
+    list.innerHTML = "";
+    let listElement;
+
+    for (let i = 0; i < this.#history.length; i++) {
+      listElement = document.createElement("li");
+      listElement.innerText = this.#history[i];
+      list.appendChild(listElement);
+    }
+  }
+}
+
+let myCalc = new Calculator();
